979c3c10c0e98ae9707d05523a7ee43a8f80eaac
Your Name <you@example.com>
2022-04-23 01:03:21 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -137,6 +137,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key) {
 }
 
 Pair * upperBound(TreeMap * tree, void* key) {
+    if (searchMap(tree, key) != NULL) return searchMap(tree, key);
     return NULL;
 }
 
