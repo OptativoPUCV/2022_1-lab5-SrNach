a017a1cad155b8d80af59ff324c54dfcba3b9029
Your Name <you@example.com>
2022-04-23 01:40:36 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -146,6 +146,8 @@ Pair * upperBound(TreeMap * tree, void* key) {
     }
     while (tree->lower_than(tree->current->left->pair->key, key) == 0){
         if (tree->current->left == NULL) break;
+        if (tree->lower_than(tree->current->left->pair->key, key))
+            return tree->current->pair;
         tree->current = tree->current->left;
         
     }
